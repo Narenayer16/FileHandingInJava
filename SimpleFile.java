899372ed8137e56da2d8be40e837cc462f968111
Naren Ayer <45704966+Narenayer16@users.noreply.github.com>
2019-08-18 00:14:46 +0545

    Add files via upload
    simple program to write any string on one file and copy that content to another file

---
+++ SimpleFile.java
@@ -0,0 +1,26 @@
+package com.naren;
+
+
+import java.io.FileOutputStream;
+import java.util.Scanner;
+
+public class SimpleFile {
+    public static void main(String[] args)
+    {
+        Scanner scanner= new Scanner(System.in);
+        try
+        {
+
+            FileOutputStream fos= new FileOutputStream("File1.txt") ;
+            System.out.println("Enter any string::");
+            String str=scanner.next();
+            byte s[]=str.getBytes();
+            fos.write(s);
+            System.out.println("File write successful.");
+
+        }catch(Exception e){
+            System.err.print("The file cannot be created.");
+
+        }
+    }
+}
